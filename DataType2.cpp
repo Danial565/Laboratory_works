d95764074c2f4fe2231a54d27bd39bf7985028ca
Danial565 <50591375+Danial565@users.noreply.github.com>
2023-09-17 18:30:23 +0200

    Add files via upload

---
+++ DataType2.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    char a = 'A';
+    short b = 270;
+    int c = 1300000;
+    long long d = 5245242425;
+    float e = 4.34;
+    double f = 4.55234;
+    bool g = true;
+    std::cout << a << " " << b << " " << c << " " << d << " " << e << " " << f << " " << g << std::endl;
+    //задание 3
+    char h = 554;
+    std::cout << h;
+}
+
