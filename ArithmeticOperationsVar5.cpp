99056116b22dff63bd3471280f0fdff21c3bd632
Danila <50591375+Danial565@users.noreply.github.com>
2023-09-17 19:41:38 +0200

    Add files via upload

---
+++ ArithmeticOperationsVar5.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	std::cout << "Введите стороны паралллелепипеда: "<<std::endl;
+
+	int a, b, c;
+	std::cin >> a >> b >> c;
+	int V=a*b*c;
+	std::cout << "Объем паралллелепипеда равен: " << V;
+}
+
+
