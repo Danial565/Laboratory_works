dfe94299d57f931c9d36efcbc0d2ec25c9cd1193
Danila <50591375+Danial565@users.noreply.github.com>
2024-05-27 14:53:55 +0200

    Add files via upload

---
+++ lab 1/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <ctime>
+#include <fstream>
+#include "Func.hpp"
+
+int main(void)
+{
+    
+    std::ifstream in("time.txt");
+    int k;
+    k=ds::getExecutionNumber(in);
+
+    std::time_t seconds = std::time(NULL);
+    tm* timeinfo = std::localtime(&seconds);
+   
+    std::ofstream out("time.txt", std::ios::app);
+    out << k << " " << timeinfo->tm_year + 1900 << " " << timeinfo->tm_mon + 1 <<
+    " " << timeinfo->tm_mday << " " << timeinfo->tm_hour <<
+    " " << timeinfo->tm_min << " " << timeinfo->tm_sec << std::endl;
+
+}
